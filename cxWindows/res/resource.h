5fe9c4ef1dfa6d04d20678657385c15488a9afcf
code1009 <code1009@naver.com>
2025-12-23 15:27:53 +0900

    초기 커밋.

---
+++ cxWindows/res/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// cxWindows.rc에서 사용되고 있습니다.
+//
+#define IDI_APP                         101
+#define IDI_APP_SMALL                   102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_MAIN                        106
+#define IDD_ABOUTBOX                    107
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
